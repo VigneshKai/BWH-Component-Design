f25e3fc8c2e9576d6c0b772bab1bf210d2725d64
RaymondTran301064 <raymond_tran-1@hotmail.com>
2020-04-16 11:52:14 +0930

    File Cleanup

--- MVP/scripts/scripts.js
+++ MVP/scripts/scripts.js
@@ -1,13 +1,14 @@
-	// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+//First script is a count down script, this was adapted between the Original Codefair Website and https://www.w3schools.com/howto/howto_js_countdown.asp
+	// Codefair Date is added to the Javascript
+var countDownDate = new Date("Nov 15, 2020 09:00:00").getTime();
 
-// Update the count down every 1 second
+// This function is to count each second 
 var x = setInterval(function() {
 
-  // Get today's date and time
+  // Get's the current time and date
   var now = new Date().getTime();
     
-  // Find the distance between now and the count down date
+  //Subtract now from codefairdate 
   var distance = countDownDate - now;
     
   // Time calculations for days, hours, minutes and seconds
@@ -16,7 +17,7 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
+  
   document.getElementById("count").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
@@ -28,14 +29,15 @@ var x = setInterval(function() {
 }, 1000);
 
 
-
+//Second script is for the collapsables adapted from https://www.w3schools.com/howto/howto_js_collapsible.asp
 function myFunction() {
+	/*If Display is  equal to none then show hidden content*/
   var x = document.getElementById("hidecontent");
   if (x.style.display === "none")
     {x.style.display = "inline-block";}
    else {
-    x.style.display = "none";
-	
+	/*If Display is not equal to none then hide the content*/
+    x.style.display = "none";	
   }
 }
 
@@ -45,7 +47,6 @@ function myFunction2() {
     {x.style.display = "inline-block";}
    else {
     x.style.display = "none";
-	
   }
 }
 
@@ -58,9 +59,12 @@ function myFunction3() {
 	
   }
 }
+
+//Second script is for the Mobile friendly Navigation Bar adapted from  https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_topnav
 function navRespon() {
   var x = document.getElementById("responsiveNav");
   if (x.className === "responsiveNav") {
+	 /*Change class name to add responsive on the end, this effectively allows it to switch css styles*/
     x.className += " responsive";
   } else {
     x.className = "responsiveNav";
